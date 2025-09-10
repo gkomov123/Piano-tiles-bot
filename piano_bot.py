b0c9bbe3f79332d629c988f1ebd019c7f21442fe
Georgi Komov <gogo5f7@gmail.com>
2025-09-10 21:31:09 +0300

    comments

--- piano_bot.py
+++ piano_bot.py
@@ -4,18 +4,21 @@ import time
 import keyboard
 import win32api, win32con
 
+# Click Function
 def click(x, y):
     win32api.SetCursorPos((x, y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
     time.sleep(0.0001)
 
+# Timeout to give time to start the web game
 time.sleep(3)
 
 y = 700
+# Main while loop
 while keyboard.is_pressed("q") == False:
 
 
-    if ag.pixel(745, y)[0] == 0:
+    if ag.pixel(745, y)[0] == 0:  # Checking if the pixel is black or not and if it is it clicks
         click(745, y)
 
     if ag.pixel(898, y)[0] == 0:
